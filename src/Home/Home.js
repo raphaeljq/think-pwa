7a8f23e4c988baab13e75585b374544509e8552a
raphaeljq <raphaeljq@gmail.com>
2018-10-01 20:51:18 -0300

    segunda linha do layout funfando . ultimo commit do dia

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -3,112 +3,131 @@ import {Container, Row, Col} from 'reactstrap'
 
 import workshop from '../imgs/workshop.png'
 const Home = () => (
-    <Container className="text-light" fluid>
-        <Row className="verdeFundo">
+
+    <main>
+        <section className="verdeFundo text-light">
+            <Container >   
+                <Row>
+                    
+                        <Col lg="6">
+                            <img src={workshop} className="fluid"/>
+                        </Col>
+                        <Col lg="6">
+                            <h2>Objetivos do Curso</h2>
+                            <p>Facilitar o dia-a-dia dos empreendedores e profissionais de marketing que desejam aprender uma metodologia de Planejamento Estratégico Digital para aplicar nos seus negócios, e em grandes ações de marketing.</p>
+                        </Col>
+                    
+                </Row>
+            </Container>    
+        </section>    
+        <section>
             <Container>
+                <Row>
+                    <Col>
+                    <h2>PÚBLICO</h2>
+                    <p>Empreendedores inovadores, profissionais de empresas, gerentes de marketing, instituições envolvidas com projetos de comunicação digital e administradores com interesse em aprimorar suas estratégias e técnicas de planejamento estratégico de seus negócios e produtos no ambiente digital.</p>
+                    </Col>
+
+                </Row>
+            </Container>
+        </section>    
+        <section>
+            <Row>
                 <Col>
-                    <img src={workshop} className="image-fluid"/>
-                </Col>
+                        <h2>CONTEÚDO </h2>
+                        <h3>1.Diagnóstico – Criando a Hipótese –Teoria e prática</h3>
+
+                        <p> Pesquisa: Quais os métodos e principais ferramentas para pesquisas.</p>
+                            <p>Cenário: Como enxergar e analisar o Cenário Digital em relação ao mercado cliente. Prognosticar tendências que podem influenciar.</p>
+                            <p>Público-Alvo: Como analisar os processos de decisão de compra do consumidor. Como definir Personas e como isso ajuda no planejamento.</p>
+                        <p>Briefing Criativo: Como indicar o melhor caminho para os demais profissionais da agência, para que estejam alinhados com os objetivos de negócio do cliente.</p>
+
+                        <h3>2.Prognóstico – Definindo a solução-Teoria e prática</h3>
+
+                    <p>Caminho Criativo: Qual é a melhor solução para o cenário do cliente e quais são os melhores caminhos a serem seguidos (Uma plataforma, campanha, produto, app e etc).</p>
+                    <p>Plano de divulgação: Quais os canais mais indicados para cada cenário, como serão os planos táticos para cada um.</p>
+                    <p>Guia Editorial: Qual é a melhor forma de trabalhar para desenvolver a melhor estratégia de abordagem focada em engajamento de usuários.</p>
+                    <p>Plano de KPI’s: Como definir o que será mensurado e quais serão os índices. Como avaliar os resultados (ROI) e recomendações melhorias.</p>
+                </Col>   
+            </Row>
+        </section>    
+        <section>
+            <Row>
                 <Col>
-                    <h2>Objetivos do Curso</h2>
-                    <p>Facilitar o dia-a-dia dos empreendedores e profissionais de marketing que desejam aprender uma metodologia de Planejamento Estratégico Digital para aplicar nos seus negócios, e em grandes ações de marketing.</p>
+                    <h3>O QUE ESTÁ INCLUSO</h3>
+                    <p> - Aula presencial em turmas com número limitado de alunos.</p>
+
+                    <p>    - Certificado de participação no curso.</p>
+
+                    <p>    - Coffeebreak </p>
+                </Col>  
+            </Row>
+        </section>
+        <section>
+            <Row>
+                <Col>
+                    <h3>INSTRUTOR</h3>
+                    <p>Nyl Barboza - CEO, Founder e gestor de marketing digital da Agência Thinking.</p>
+
+                    <p>Designer gráfico e de produto, especialista em marketing digital.</p>
+
+                    <p>Professor universitário das disciplinas marketing e branding.</p>
                 </Col>
-            </Container> 
-        </Row>
-        <Row>
-            <Col>
-            <h2>PÚBLICO</h2>
-            <p>Empreendedores inovadores, profissionais de empresas, gerentes de marketing, instituições envolvidas com projetos de comunicação digital e administradores com interesse em aprimorar suas estratégias e técnicas de planejamento estratégico de seus negócios e produtos no ambiente digital.</p>
+            </Row>      
+        </section>    
+        <section>
+            <Row>
+                <Col>
 
-            </Col>
-        </Row>
-        <Row>
-           <Col>
-                <h2>CONTEÚDO </h2>
-                <h3>1.Diagnóstico – Criando a Hipótese –Teoria e prática</h3>
+                    <h3>INFORMAÇÕES SOBRE INSCRIÇÕES E MATRÍCULAS</h3>
+                    <p>1º Lote: R$ 177,00 até 05/10.</p>
 
-                <p> Pesquisa: Quais os métodos e principais ferramentas para pesquisas.</p>
-                    <p>Cenário: Como enxergar e analisar o Cenário Digital em relação ao mercado cliente. Prognosticar tendências que podem influenciar.</p>
-                    <p>Público-Alvo: Como analisar os processos de decisão de compra do consumidor. Como definir Personas e como isso ajuda no planejamento.</p>
-                <p>Briefing Criativo: Como indicar o melhor caminho para os demais profissionais da agência, para que estejam alinhados com os objetivos de negócio do cliente.</p>
+                    <p>2º Lote: não liberado.</p>
 
-                <h3>2.Prognóstico – Definindo a solução-Teoria e prática</h3>
+                    <p>3º Lote: não liberado.</p>
+                </Col>
+            </Row>    
+        </section>
+        <section>
+            <Row>
+                <Col>
+                    <p>Basta clicar no botão "Inscreva-se", aqui do lado esquerdo. Após logar no nosso sistema e preencher seus dados, você pode confirmar a sua matrícula através de:</p>
 
-            <p>Caminho Criativo: Qual é a melhor solução para o cenário do cliente e quais são os melhores caminhos a serem seguidos (Uma plataforma, campanha, produto, app e etc).</p>
-            <p>Plano de divulgação: Quais os canais mais indicados para cada cenário, como serão os planos táticos para cada um.</p>
-            <p>Guia Editorial: Qual é a melhor forma de trabalhar para desenvolver a melhor estratégia de abordagem focada em engajamento de usuários.</p>
-            <p>Plano de KPI’s: Como definir o que será mensurado e quais serão os índices. Como avaliar os resultados (ROI) e recomendações melhorias.</p>
-           </Col>   
-        </Row>
-        <Row>
-            <Col>
-                <h3>O QUE ESTÁ INCLUSO</h3>
-                <p> - Aula presencial em turmas com número limitado de alunos.</p>
+                    <p>Boleto Bancário (apenas pagamento à vista).</p>
 
-                <p>    - Certificado de participação no curso.</p>
+                    <p>Cartão de Crédito - Parcelamento em até 12x sem juros. </p>
+                </Col>
 
-                <p>    - Coffeebreak </p>
-            </Col>  
-        </Row>
-          
-        <Row>
-            <Col>
-                <h3>INSTRUTOR</h3>
-                <p>Nyl Barboza - CEO, Founder e gestor de marketing digital da Agência Thinking.</p>
+            </Row>
+        </section>    
+        <section>
+            <Row>
+                <Col>
+                    <h2>INFORMAÇÕES ADICIONAIS</h2>
 
-                <p>Designer gráfico e de produto, especialista em marketing digital.</p>
+                    <p>Caso você tenha se matriculado e não possa mais comparecer ao curso na respectiva turma, indique um substituto direto. Você pode fazer isso até o dia anterior do início do curso.  :)</p>
 
-                <p>Professor universitário das disciplinas marketing e branding.</p>
-            </Col>
-        </Row>      
-  
-        <Row>
-            <Col>
+                    <p>Porém, se você quiser cancelar a sua participação na turma e não indicar um substituto, basta nos avisar por e-mail com antecedência maior que 7 dias para o início da turma do seu curso. Nesse caso, nós infelizmente vamos precisar descontar uma taxa administrativa nos seguintes valores:</p>
 
-                <h3>INFORMAÇÕES SOBRE INSCRIÇÕES E MATRÍCULAS</h3>
-                <p>1º Lote: R$ 177,00 até 05/10.</p>
+                    <p>30% se você quiser o dinheiro de volta. 15% se preferir usar o crédito em qualquer outro curso da BMS (incluindo, em caso de disponibilidade, novas turmas do mesmo curso)</p>
 
-                <p>2º Lote: não liberado.</p>
+                    <p>Cancelamentos com menos de 7 dias para o início da turma, terão as taxas acima duplicadas. Faltas e ausências durante a(s) aula(s), infelizmente, não terão reposições, créditos e/ou devoluções. :(</p>
 
-                <p>3º Lote: não liberado.</p>
-            </Col>
-        </Row>    
+                </Col>
+            </Row>
+        </section>    
+        <section>
+            <Row>
+                <Col>
+                <h2>SOBROU ALGUMA DÚVIDA?</h2>
+                    <p>Quer saber mais sobre algo? Não tem certeza se é isso mesmo? Ficou com alguma dúvida? </p>
 
-         <Row>
-             <Col>
-                <p>Basta clicar no botão "Inscreva-se", aqui do lado esquerdo. Após logar no nosso sistema e preencher seus dados, você pode confirmar a sua matrícula através de:</p>
+                    <p>Se você quer fazer o curso mas quer conversar antes, não deixe de entrar em contato. Fale conosco através do nosso e-mail, do Facebook ou do telefone que está aqui no rodapé da página. Só não temos atendimento presencial, mas meio de contato é o que não vai faltar. :)</p>
+                </Col>        
+            </Row>    
+        </section>                
 
-                <p>Boleto Bancário (apenas pagamento à vista).</p>
-
-                <p>Cartão de Crédito - Parcelamento em até 12x sem juros. </p>
-            </Col>
-
-        </Row>
-        <Row>
-            <Col>
-                <h2>INFORMAÇÕES ADICIONAIS</h2>
-
-                <p>Caso você tenha se matriculado e não possa mais comparecer ao curso na respectiva turma, indique um substituto direto. Você pode fazer isso até o dia anterior do início do curso.  :)</p>
-
-                <p>Porém, se você quiser cancelar a sua participação na turma e não indicar um substituto, basta nos avisar por e-mail com antecedência maior que 7 dias para o início da turma do seu curso. Nesse caso, nós infelizmente vamos precisar descontar uma taxa administrativa nos seguintes valores:</p>
-
-                <p>30% se você quiser o dinheiro de volta. 15% se preferir usar o crédito em qualquer outro curso da BMS (incluindo, em caso de disponibilidade, novas turmas do mesmo curso)</p>
-
-                <p>Cancelamentos com menos de 7 dias para o início da turma, terão as taxas acima duplicadas. Faltas e ausências durante a(s) aula(s), infelizmente, não terão reposições, créditos e/ou devoluções. :(</p>
-
-            </Col>
-        </Row>
-        <Row>
-            <Col>
-            <h2>SOBROU ALGUMA DÚVIDA?</h2>
-                <p>Quer saber mais sobre algo? Não tem certeza se é isso mesmo? Ficou com alguma dúvida? </p>
-
-                <p>Se você quer fazer o curso mas quer conversar antes, não deixe de entrar em contato. Fale conosco através do nosso e-mail, do Facebook ou do telefone que está aqui no rodapé da página. Só não temos atendimento presencial, mas meio de contato é o que não vai faltar. :)</p>
-            </Col>        
-    </Row>    
-
-
-    </Container>
+    </main>
 
 
 )
--- src/header/Header.js
+++ src/header/Header.js
@@ -8,9 +8,9 @@ const Header = (props) =>{
         <Jumbotron data-css3 fluid>
           <Container data-centro>
             
-          <div class="hero-text">
+          <div className="hero hero-text">
             {/**<h1 class="display-4">Workshop no Amazoom Cowork</h1>**/}
-            <p className="display-4 tituloHeader"><span className="oswald">PLA<br/>NEJA<br/>MENTO<br/></span> </p><p><span class="verdeTexto tituloHeaderVerde"> DE MARKETING DIGITAL</span></p>
+            <p className="display-4 tituloHeader"><span className="oswald">PLA<br/>NEJA<br/>MENTO<br/></span> </p><p><span className="verdeTexto tituloHeaderVerde"> DE MARKETING DIGITAL</span></p>
             <p>Aprenda passo-a-passo como planejar sua campanha digital de sucesso, estabelecendo metas e resultados que você quer alcançar para seus negócios.</p>
             <Button color="success" className="verdeFundo">Inscreva-se!</Button>
           </div>
