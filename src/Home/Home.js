f91ace9eac84201a6fb12dddd4723d831887402f
raphaeljq <raphaeljq@gmail.com>
2018-10-05 17:26:29 -0300

    adicionadas linhas até a linha do instrutor

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,6 +2,8 @@ import React from 'react'
 import {Container, Row, Col} from 'reactstrap'
 
 import workshop from '../img/workshop.png'
+import datahora from '../img/data-hora.png'
+import instrutor from '../img/instrutor.png'
 
 const Home = () => (
 
@@ -29,7 +31,65 @@ const Home = () => (
 
                 </Row>
             </Container>
-        </section>     
+        </section>
+        <section className="text-light keyfundo">
+            <Container>
+                <Row>
+                    <Col className="text-center">
+                    
+                    <p>O Marketing é uma das chaves
+que você vai precisar pra abrir as portas para
+o mercado. Quer fazer sua marca ser mais
+conhecida, desejada e vista positivamente
+pelos consumidores?
+Chega pra cá!</p>
+                    </Col>
+
+                </Row>
+            </Container>
+        </section>
+        <section className="text-light fundoEscuro">
+            <Container>
+                <Row>
+                    <Col className="text-center">
+                        <img src={datahora}  className="img-fluid"/>
+                   
+                    </Col>
+
+                </Row>
+            </Container>
+        </section>
+        <section className="text-light fundoTopicos">
+            <Container>
+                <Row>
+                    <Col className="text-center">
+                        <h2>Tópicos abordados</h2>
+                        <ul className="text-left">
+                            <li>Posicionamento de Marca</li>
+                            <li>Marketing</li>
+                            <li>Funil de Vendas</li>
+                            <li>Canais estratégicos</li>
+                            <li>Relacionamento Digital</li>
+                            <li>Vendas</li>
+                        </ul>
+                   
+                    </Col>
+
+                </Row>
+            </Container>
+        </section>
+        <section className="fundoInstrutor">
+            <Container>
+                <Row>
+                    <Col xs="12" className="text-center fotoInstrutor">
+                        <img src={instrutor} />
+                   
+                    </Col>
+                    <Col xs="12" className="text-center"><h3>O Instrutor</h3></Col>
+
+                </Row>
+            </Container>
+        </section>                 
              
 
     </main>
